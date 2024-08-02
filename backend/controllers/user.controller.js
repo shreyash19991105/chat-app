3c859db4ab8c199fae4340c508533eaedcf178eb
shreyash <shreyashsonkusare87@gmail.com>
2024-08-02 10:03:55 +0530

    1st part completed backend

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,15 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const loggedInuserId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInuserId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in getUsersForSidebar :", error);
+    res.status(500).json({ error: "Internal server error" });
+  }
+};
